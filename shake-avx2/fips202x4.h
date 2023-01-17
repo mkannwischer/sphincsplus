6b06921ac178039c0a3327b8e3dffc581e3576af
Thom Wiggers <thom@thomwiggers.nl>
2023-01-17 19:12:53 +0100

    namespace shake128x4

--- shake-avx2/fips202x4.h
+++ shake-avx2/fips202x4.h
@@ -3,6 +3,9 @@
 
 #include <immintrin.h>
 
+#include "params.h"
+
+#define shake128x4 SPX_NAMESPACE(shake128x4)
 void shake128x4(unsigned char *out0,
                 unsigned char *out1,
                 unsigned char *out2,
@@ -12,6 +15,7 @@ void shake128x4(unsigned char *out0,
                 unsigned char *in2,
                 unsigned char *in3, unsigned long long inlen);
 
+#define shake256x4 SPX_NAMESPACE(shake256x4)
 void shake256x4(unsigned char *out0,
                 unsigned char *out1,
                 unsigned char *out2,
