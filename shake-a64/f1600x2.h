ae132f7bf9f90205834c1cbdf5cfb9187d51f48e
Matthias J. Kannwischer <matthias@kannwischer.eu>
2025-02-17 14:20:56 +0800

    add namespacing to fips202x2

--- shake-a64/f1600x2.h
+++ shake-a64/f1600x2.h
@@ -2,7 +2,9 @@
 #define SPX_F1600X2_H
 
 #include <stdint.h>
-
+#include "context.h"
+#include "params.h"
+#define f1600_RC SPX_NAMESPACE(f1600_RC)
 extern uint64_t f1600_RC[24];
 extern void _f1600x2(uint64_t* a, uint64_t* rc);
 
